361305dedc8509a4beecbb4d5b62607e6a2dc1e8
viji <viji@Linux.(none)>
2012-08-20 11:50:16 +0530

    User Controller

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,26 @@
+class UserController < ApplicationController
+  def Index
+	
+  end
+  
+  def signupAPI
+  
+  end
+  
+  def emailExistsAPI
+  end
+  
+  def createAPI
+  end
+  
+  def signupTokenExistsAPI
+  end
+  
+  def scoreboardAPI
+  end
+  
+  def 
+  def demo
+	render("demo");
+  end	
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'a45fb9eab048b261f5112a1fa66577358871f0ae85a25caa5397f737d87372dba280c70f6689c90b3e3b2b078b00d493bf3fb2a5827e3764fedc854bdceefc79'
